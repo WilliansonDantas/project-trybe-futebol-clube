5c39b35154817eb6ad7c9cdfa0893ca60a1ca57f
Willianson Dantas <williansondantas@gmail.com>
2022-10-29 13:05:01 -0300

    feat: req 01, 14 e 18, correspondentes as migrations e models das tabelas do projeto: users, teams e matches

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,68 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'home_team',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'home_team_goals',
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'away_team',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+    field: 'in_progress',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Matches;
---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+    field: 'team_name',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
