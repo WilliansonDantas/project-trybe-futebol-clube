e1ea7ed66c529554c355675f12deb8784c402442
Willianson Dantas <williansondantas@gmail.com>
2022-11-03 22:22:46 -0300

    fix: retirada dos erros de lint

---
+++ app/backend/src/middlewares/validadedPassword.ts
@@ -0,0 +1,10 @@
+// const validadedPassword = (req, res, next) => {
+//     const { password } = req.body;
+
+//     if (password === undefined) {
+//         return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+//     }
+//     next();
+// };
+
+// module.exports = validadedPassword;
