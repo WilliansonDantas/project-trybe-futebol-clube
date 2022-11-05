714fc5027e01cb19cd54750db979a9b4576b1007
Willianson Dantas <williansondantas@gmail.com>
2022-11-05 18:41:56 -0300

    req25: verificação se os dois times inseridos na rota post matches são iguais, dessa forma, impedindo a inserção do jogo no banco de dados

--- app/backend/src/controllers/MatchesPostController.ts
+++ app/backend/src/controllers/MatchesPostController.ts
@@ -14,19 +14,17 @@ export default class MatchesPostController {
   }
 
   async create(req: Request, res: Response) {
+    const { homeTeam, awayTeam } = req.body;
+    // const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = req.body;
+    MatchesPostController.teamsDifferent(homeTeam, awayTeam);
     const token = req.header('Authorization') as string;
     this.decodify(token);
-    console.log(req.body);
     const inProgress = true;
-    const newItem = await this._service
-      .create(req.body, inProgress);
-    console.log(newItem);
+    const newItem = await this._service.create(req.body, inProgress);
     return res.status(201).json(newItem);
   }
 
   private decodify(token: string) {
-    console.log(secret);
-
     try {
       const payloadVerify = this._jwt.verify(token, secret) as jwt.JwtPayload;
       return payloadVerify;
@@ -34,4 +32,22 @@ export default class MatchesPostController {
       throw new HttpException(404, 'Expired or invalid token');
     }
   }
+
+  private static teamsDifferent(homeTeam: number, awayTeam: number) {
+    if (Number(homeTeam) === Number(awayTeam)) {
+      throw new HttpException(422, 'It is not possible to create a match with two equal teams');
+    }
+  }
+
+  // private static emailValidated(email: string) {
+  //   if (email.match(/\S+@\S+\.\S+/) === null) {
+  //     throw new HttpException(401, error401);
+  //   }
+  // }
+
+  // private static passwordValidated(password: string) {
+  //   if (password.length <= 6) {
+  //     throw new HttpException(401, error401);
+  //   }
+  // }
 }
